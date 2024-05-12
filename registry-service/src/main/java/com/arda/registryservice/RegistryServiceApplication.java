6b3ec87793b6011524117200fde2b4fe482297cb
arieelbernal <arieelbernal@gmail.com>
2024-05-12 04:58:08 -0300

    fix config

--- registry-service/src/main/java/com/arda/registryservice/RegistryServiceApplication.java
+++ registry-service/src/main/java/com/arda/registryservice/RegistryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.registryservice;
+package com.arda.registryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- registry-service/src/test/java/com/arda/registryservice/RegistryServiceApplicationTests.java
+++ registry-service/src/test/java/com/arda/registryservice/RegistryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.registryservice;
+package com.arda.registryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
